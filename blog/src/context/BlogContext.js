99119482d1e634cd97f6d651b86f274d35c9418a
Kleber Motta <kleber_motta@Klebers-iMac.local>
2019-09-21 02:53:11 -0300

    Alterações

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import createDataContext from './createDataContext';
 
-const BlogContext = React.createContext();
-
-export const BlogProvider = ({ children }) => {
-  const blogPosts = [{ title: 'Blog Post #1' }, { title: 'Blog Post #2' }];
-  let teste = 0;
-  return (
-    <BlogContext.Provider value={blogPosts} tst={teste} call={() => {}}>
-      {children}
-    </BlogContext.Provider>
-  );
+const blogReducer = (state, action) => {
+  switch(action.type) {
+    case "delete_blogpost":
+      return state.filter( (blogPost) =>
+        blogPost.id !== action.payload
+      );
+    case "add_blogpost":
+      return [...state, 
+        { 
+          id: Math.floor(Math.random() * 99999), 
+          title: action.payload.title,
+          content: action.payload.content 
+        }];
+    default:
+      return state;
+  }
 };
 
-export default BlogContext;
+const addBlogPost = (dispatch) => {
+  return (title, content) => {
+    dispatch({type: 'add_blogpost', payload: {title, content}});
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({type: 'delete_blogpost', payload: id});
+  };
+};
+
+export const {Context, Provider} = createDataContext(
+  blogReducer, 
+  {addBlogPost, deleteBlogPost}, 
+  [] 
+);
